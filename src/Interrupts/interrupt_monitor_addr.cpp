84a11b8320afcd56c4522d8b0b735584316e8917
carllocos <carlosj.rojascastillo@gmail.com>
2023-10-18 22:28:20 +0200

    Add interrupt Monitor Addr
    - handle a request
    - deserialize a request
    - serialize a response (only declared)
    - send response (only declared)

---
+++ src/Interrupts/interrupt_monitor_addr.cpp
@@ -0,0 +1,80 @@
+#include "interrupt_monitor_addr.h"
+
+#include "../Utils/util.h"
+#include "interrupts.h"
+
+/*
+ * Declaration private functions
+ */
+bool registerMonitorAddrAction(InstrumentationManager &manager, Module &m,
+                               const MonitorAddrRequest &request,
+                               uint8_t &error_code);
+
+/*
+ * Public functions
+ */
+
+void Interrupt_MonitorAddr_handle_request(const Channel &channel,
+                                          InstrumentationManager &manager,
+                                          Module *m, uint8_t *encoded_request) {
+    Action action{};
+    MonitorAddrRequest request;
+    request.action = &action;
+
+    MonitorAddrResponse response;
+    uint8_t error{};
+
+    if (Interrupt_MonitorAddr_deserialize_request(request, encoded_request,
+                                                  error) &&
+        registerMonitorAddrAction(manager, *m, request, error)) {
+        response.type = INTERRUPT_RESPONSE_TYPE_SUCCESS;
+    } else {
+        response.type = INTERRUPT_RESPONSE_TYPE_ERROR;
+        response.error_code = error;
+    }
+
+    Interrupt_MonitorAddr_send_response(channel, response);
+}
+
+bool Interrupt_MonitorAddr_deserialize_request(MonitorAddrRequest &dest,
+                                               uint8_t *encoded_request,
+                                               uint8_t &error_code) {
+    // format: InterruptNr (1 byte)| addr (LEB32) | InstrumentMoment (1 byte) |
+    // action
+
+    uint8_t *data = encoded_request;
+    if (*data++ != interruptMonitorAddr) {
+        error_code = MONITOR_ADDR_ERROR_CODE_REQUEST_HAS_WRONG_INTERRUPT_NR;
+        return false;
+    }
+    dest.addr = read_LEB_32(&data);
+    dest.moment = (InstrumentMoment)*data++;
+    switch (dest.moment) {
+        case InstrumentBefore:
+        case InstrumentAfter:
+            break;
+        default:
+            error_code = MONITOR_ADDR_ERROR_CODE_REQUEST_HAS_UN_EXISTING_MOMENT;
+            return false;
+    }
+
+    return Actions_deserialize_action(*dest.action, &data, error_code);
+}
+
+char *Interrupt_MonitorAddr_serialize_response(
+    const MonitorAddrResponse &response, char *dest) {}
+
+void Interrupt_MonitorAddr_send_response(const Channel &channel,
+                                         const MonitorAddrResponse &response) {
+    char buffer[100]{};
+    Interrupt_MonitorAddr_serialize_response(response, buffer);
+    channel.write(buffer);
+}
+
+/*
+ * Private functions
+ */
+
+bool registerMonitorAddrAction(InstrumentationManager &manager, Module &m,
+                               const MonitorAddrRequest &request,
+                               uint8_t &error_code) {}
---
+++ src/Interrupts/interrupt_monitor_addr.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <stddef.h>
+
+#include "../Instrumentation/action.h"
+#include "../Instrumentation/instrumentation.h"
+#include "../WARDuino/structs.h"
+
+#define MONITOR_ADDR_ERROR_CODE_REQUEST_HAS_WRONG_INTERRUPT_NR 1
+#define MONITOR_ADDR_ERROR_CODE_REQUEST_HAS_UN_EXISTING_MOMENT 2
+
+enum InstrumentMoment {
+    InstrumentBefore = 0x01,
+    InstrumentAfter = 0x02,
+};
+
+typedef struct {
+    uint32_t addr{};
+    InstrumentMoment moment{};
+    Action *action{};
+} MonitorAddrRequest;
+
+typedef struct {
+    uint8_t type{};
+    uint8_t error_code{};
+} MonitorAddrResponse;
+
+void Interrupt_MonitorAddr_handle_request(const Channel &channel,
+                                          InstrumentationManager &manager,
+                                          Module *m, uint8_t *encoded_request);
+
+bool Interrupt_MonitorAddr_deserialize_request(MonitorAddrRequest &request,
+                                               uint8_t *encoded_request,
+                                               uint8_t &error_code);
+
+char *Interrupt_MonitorAddr_serialize_response(
+    const MonitorAddrResponse &response, char *dest);
+
+void Interrupt_MonitorAddr_send_response(const Channel &channel,
+                                         const MonitorAddrResponse &response);
