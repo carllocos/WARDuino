7f7719248287933228f2256e0089946a44c0c900
carllocos <carlosj.rojascastillo@gmail.com>
2023-10-24 16:47:41 +0200

    implement interrupt_monitor_event handler

--- src/Interrupts/interrupt_monitor_event.cpp
+++ src/Interrupts/interrupt_monitor_event.cpp
@@ -1,10 +1,19 @@
 #include "interrupt_monitor_event.h"
 
+#include "../Interrupts/interrupt_response.h"
 #include "../Utils/macros.h"
 
 void Interrupt_Monitor_Event_handle_request(const Channel &requester, Module &m,
                                             uint8_t *encoded_request) {
-    FATAL("TODO");
+    MonitorEventRequest request{};
+    MonitorEventResponse response{};
+    if (Interrupt_Monitor_Event_deserialize_request(request, encoded_request,
+                                                    response.error_code)) {
+        response.type = INTERRUPT_RESPONSE_TYPE_SUCCESS;
+    } else {
+        response.type = INTERRUPT_RESPONSE_TYPE_ERROR;
+    }
+    Interrupt_MonitorEvent_send_response(requester, response);
 }
 
 bool Interrupt_Monitor_Event_deserialize_request(MonitorEventRequest &dest,
