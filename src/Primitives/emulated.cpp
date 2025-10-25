b0969dcf27c651f06beeef15b3721318be771a7b
Carlos Rojas <23368410+carllocos@users.noreply.github.com>
2025-10-25 17:11:20 +0200

    fix clang format

--- src/Primitives/emulated.cpp
+++ src/Primitives/emulated.cpp
@@ -434,7 +434,7 @@ def_prim(write_spi_bytes_16, twoToNoneU32) {
 }
 
 def_prim(subscribe_interrupt, threeToNoneU32) {
-    uint8_t pin = arg2.uint32;   // GPIOPin
+    uint8_t pin = arg2.uint32;    // GPIOPin
     uint32_t tidx = arg1.uint32;  // Table Idx pointing to Callback function
     uint8_t mode = arg0.uint32;
 
@@ -464,7 +464,6 @@ def_prim(chip_ledc_setup, threeToNoneU32) {
     pop_args(3);
     return true;
 }
-
 
 def_prim(chip_ledc_attach_pin, twoToNoneU32) {
     uint32_t pin = arg1.uint32;
