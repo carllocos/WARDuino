a0c9d8d560864170be8610ce6c15b432fb9881b6
Carlos Rojas <23368410+carllocos@users.noreply.github.com>
2025-12-10 16:06:42 +0100

    refactor: remove unneeded code

--- src/Instrumentation/hook.h
+++ src/Instrumentation/hook.h
@@ -56,9 +56,6 @@ struct Hook {
  */
 Hook *Hooks_add_and_sort(Hook *hooks, Hook *hook_to_add);
 
-Hook *Hooks_nextScheduledHook(Hook *sorted_hooks,
-                              const LogicalClock &currentTime);
-
 bool Hook_isScheduledForNow(LogicalClock ct, Schedule s);
 
 bool Hooks_isHookWaitingForEvent(Hook *sorted_hooks,
